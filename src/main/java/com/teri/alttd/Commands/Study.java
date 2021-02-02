aefbac008676028d7c6320d04114a390d6c7eeea
Sti-jn <stijn.d2000@hotmail.com>
2021-02-02 22:40:58 +0100

    Added comments to clarify what everything does

--- src/main/java/com/teri/alttd/Commands/Study.java
+++ src/main/java/com/teri/alttd/Commands/Study.java
@@ -8,12 +8,22 @@ public class Study {
     String[] args;
     GuildMessageReceivedEvent event;
 
+    /**
+     * Initialize the Study command, store the command, arguments, and the event.
+     * @param command Command that was executed.
+     * @param args Arguments that were given with that command.
+     * @param event The event that started the command.
+     */
     public Study(String command, String[] args, GuildMessageReceivedEvent event) {
         this.command = command;
         this.args = args;
         this.event = event;
     }
 
+    /**
+     * Runs the Study command
+     * @param prefix The prefix that was used to start this command
+     */
     public void runCommand(char prefix){
         TextChannel channel = event.getChannel();
         if (args.length < 6){
