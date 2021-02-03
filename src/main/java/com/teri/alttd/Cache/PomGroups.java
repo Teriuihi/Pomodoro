c67a2dd19988e21a49423c21f7d6255ae128f8a8
Sti-jn <stijn.d2000@hotmail.com>
2021-02-03 07:31:14 +0100

    added pom cache

---
+++ src/main/java/com/teri/alttd/Cache/PomGroups.java
@@ -0,0 +1,52 @@
+package com.teri.alttd.Cache;
+
+import com.teri.alttd.Objects.Pom;
+import com.teri.alttd.Queries.PomQueries;
+
+import java.util.HashMap;
+
+public class PomGroups {
+    private static final HashMap<Integer, Pom> pomGroups = new HashMap<>(); //All admin groups for all guilds that have added admin groups.
+
+    /**
+     * Check if a specific group in a specific guild is a pom group.
+     * @param pomId the pom to check for.
+     * @return if a group is in a specific guild.
+     */
+    public static boolean inPom(int pomId, long userId){
+        return pomGroups.get(pomId).hasUser(userId);
+    }
+
+    /**
+     * Get the pom with the specified id from the pomGroups map
+     * @param pomId id of the pom you want to retrieve
+     * @return the pom belonging to the pomId
+     */
+    public static Pom getPom(long pomId) {
+        return pomGroups.get(pomId);
+    }
+
+    /**
+     * Remove the pom group.
+     * @param pomId the id of the pom to remove.
+     */
+    public static void removePom(int pomId){
+        pomGroups.remove(pomId);
+        PomQueries.deletePom(pomId);
+    }
+
+    /**
+     * Add the pom group, it should already be in the database since you can't have the pomId without a database entry.
+     * @param pomId group to add.
+     */
+    public static void addPom(int pomId, Pom pom){
+        pomGroups.put(pomId, pom);
+    }
+
+    /**
+     * Load all pom groups.
+     */
+    public static void loadPomGroups(){
+        pomGroups.putAll(PomQueries.loadAllPoms());
+    }
+}
