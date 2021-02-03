fbf4bb9d6b189b0c2e2b787f4fe1e948683cce81
Sti-jn <stijn.d2000@hotmail.com>
2021-02-03 07:36:03 +0100

    Initial attempt at creating the pom scheduler

---
+++ src/main/java/com/teri/alttd/Scheduling/PomScheduler.java
@@ -0,0 +1,71 @@
+package com.teri.alttd.Scheduling;
+
+import com.teri.alttd.FileManagement.Log;
+import com.teri.alttd.Main;
+import com.teri.alttd.Objects.Pom;
+import org.quartz.JobDetail;
+import org.quartz.JobKey;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SimpleTrigger;
+
+import java.util.Date;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class PomScheduler {
+
+    private final Pom pom;
+    private final int pomId;
+
+    public PomScheduler(Pom pom, int pomId) {
+        this.pom = pom;
+        this.pomId = pomId;
+    }
+
+    /**
+     * Schedule a new job
+     */
+    public void schedule(){
+        try {
+            Scheduler scheduler = Main.schedFact.getScheduler();
+            JobDetail jobDetail = getJobDetail();
+
+            scheduler.start();
+            Date time;
+            if (pom.sessionActive()){
+                time = pom.getNextBreak();
+            } else {
+                time = pom.getNextSession();
+            }
+
+            scheduler.scheduleJob(jobDetail, getTrigger(jobDetail, time));
+        } catch (SchedulerException e) {
+            new Log(Log.LogType.ERROR).appendLog(e.getStackTrace());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Create a JobDetail
+     * @return the created JobDetail
+     */
+    private JobDetail getJobDetail(){
+        return newJob(PomJob.class).withIdentity(new JobKey(String.valueOf(pomId), "pom")).build();
+    }
+
+    /**
+     * Create a SimpleTrigger
+     * @param job Job to create the trigger with
+     * @return the SimpleTrigger after it's created
+     */
+    private SimpleTrigger getTrigger(JobDetail job, Date time){
+        return (SimpleTrigger) newTrigger()
+                .withIdentity(String.valueOf(pomId), "pom")
+                .startAt(time)
+                .forJob(job)
+                .usingJobData("pomId", pomId)
+                .build();
+    }
+}
