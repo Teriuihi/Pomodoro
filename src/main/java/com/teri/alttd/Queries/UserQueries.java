abbc9c98b03ccdfbf26eeacffe6e2ae397f9c55e
Sti-jn <stijn.d2000@hotmail.com>
2021-02-03 07:35:24 +0100

    Added user queries

---
+++ src/main/java/com/teri/alttd/Queries/UserQueries.java
@@ -0,0 +1,53 @@
+package com.teri.alttd.Queries;
+
+import com.teri.alttd.Database.Database;
+import com.teri.alttd.FileManagement.Log;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UserQueries {
+    private final static String addUserQuery = "INSERT INTO users (user_id, pom_id) VALUES (?, ?)";
+    private final static String deleteUserQuery = "DELETE FROM users WHERE user_id = ?";
+    private final static String deleteAllUsersQuery = "DELETE FROM users WHERE pom_id = ?";
+
+    public static void addUser(long userId, int pomId){
+        try {
+            PreparedStatement statement = Database.connection.prepareStatement(addUserQuery);
+
+            statement.setLong(1, userId);
+            statement.setInt(2, pomId);
+
+            statement.execute();
+        } catch (SQLException e) {
+            new Log(Log.LogType.SQL).appendLog(e.getStackTrace());
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteUser(long userId){
+        try {
+            PreparedStatement statement = Database.connection.prepareStatement(deleteUserQuery);
+
+            statement.setLong(1, userId);
+
+            statement.execute();
+        } catch (SQLException e) {
+            new Log(Log.LogType.SQL).appendLog(e.getStackTrace());
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteAllUser(int pomId){
+        try {
+            PreparedStatement statement = Database.connection.prepareStatement(deleteAllUsersQuery);
+
+            statement.setInt(1, pomId);
+
+            statement.execute();
+        } catch (SQLException e) {
+            new Log(Log.LogType.SQL).appendLog(e.getStackTrace());
+            e.printStackTrace();
+        }
+    }
+}
