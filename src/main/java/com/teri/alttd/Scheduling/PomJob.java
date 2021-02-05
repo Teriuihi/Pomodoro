46624cb450c789af54b7f4b3e4473056dd0cd37e
Sti-jn <stijn.d2000@hotmail.com>
2021-02-05 19:34:05 +0100

    Mention the role when sending a pom message

--- src/main/java/com/teri/alttd/Scheduling/PomJob.java
+++ src/main/java/com/teri/alttd/Scheduling/PomJob.java
@@ -6,6 +6,7 @@ import com.teri.alttd.Objects.Pom;
 import com.teri.alttd.Utilities.Utils;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.TextChannel;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -28,10 +29,15 @@ public class PomJob implements Job {
         if (Utils.isNull(this, channel, "getting textChannel" + pom.getChannelId())) return;
 
         Member member = guild.getMemberById(pom.getOwnerId());
+        Role role = guild.getRoleById(pom.getRoleId());
         if (member == null){
-            guild.retrieveMemberById(pom.getOwnerId()).queue(retrievedMember -> sendMessage(channel, retrievedMember, pom));
+            if (role != null){
+                guild.retrieveMemberById(pom.getOwnerId()).queue(retrievedMember -> sendMessage(channel, retrievedMember, pom, role));
+            }
         } else {
-            sendMessage(channel, member, pom);
+            if (role != null){
+                sendMessage(channel, member, pom, role);
+            }
         }
     }
 
@@ -40,12 +46,13 @@ public class PomJob implements Job {
      * @param channel Channel to send the message in
      * @param member Member who created the pom
      * @param pom The pom we are doing this for
+     * @param role The role for those participating in the pom
      */
-    private void sendMessage(TextChannel channel, Member member, Pom pom){
+    private void sendMessage(TextChannel channel, Member member, Pom pom, Role role){
         Utils.isNull(this, member, "retrieving member");
-        channel.sendMessage(pom.getEmbeddedMessage(member, pom.sessionActive())).queue();
+        channel.sendMessage(role.getAsMention()).embed(pom.getEmbeddedMessage(member, pom.sessionActive())).queue();
 
-        if (pom.getCurrentCycle() >= pom.getCyclesAmount()){
+        if (pom.getCurrentSession() >= pom.getSessionAmount()){
             pom.delete();
         } else {
             if (pom.sessionActive()){
